0abaee1a8f6b3d1e92ab341580a155ca09d5ecc8
saber <saber30092@gmail.com>
2021-05-06 10:00:07 +0800

    測試版1

---
+++ DrinkAge_1.0/LangControl.cs
@@ -0,0 +1,29 @@
+﻿using DrinkAge_1._0;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrinkAge
+{
+    public partial class 彥良主控台logic : Form
+    {
+        DrinkAgeEntities dbContext = new DrinkAgeEntities();
+        public 彥良主控台logic()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+
+        }
+    }
+}
